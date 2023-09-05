92ea786d057d5a60f05a2478bab469e2ef8693c7
Sar-dor <sardorernazarov4555@gmail.com>
2023-09-05 09:23:40 +0500

    Heroku kodi yozildi

--- data/config.py
+++ data/config.py
@@ -15,4 +15,4 @@ import os
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
 ADMINS = list(os.environ.get("ADMINS"))  # adminlar ro'yxati
-IP = str(os.environ.get("ip"))  # Xosting ip manzil
+IP = str(os.environ.get("ip"))  # Xosting ip manzil.
